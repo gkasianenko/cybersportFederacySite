c0e766f1b2cf6535f700a5fd86980a9faeb40d06
gkasianenko <g.kasianenko@yandex.ru>
2023-03-15 13:33:25 +0300

    swiper first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,9 +6,13 @@ module.exports = {
       colors: {
         mainbg: "#03001C",
       },
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(185.06% 308.64% at 159.52% 253.76%, rgba(227, 218, 253, 0.288) 30.21%, rgba(219, 207, 255, 0.873) 68.49%)",
+      },
     },
     fontFamily: {
       manrope: ["Manrope", "Arial", "sans-serif"],
+      spgrotesk: ["Space\\ Grotesk", "Arial", "sans-serif"],
     },
   },
   plugins: [],
